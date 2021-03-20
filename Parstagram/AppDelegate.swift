719722740f927742e0ed46d243fa4cc382ec7971
Erin Byrd <ebbyrd@email.unc.edu>
2021-03-20 02:26:27 -0400

    Parstagram first commit

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,26 +6,39 @@
 //
 
 import UIKit
+import Parse
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
 
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration
+        {
+                    $0.applicationId = "nTL1WCqm6cvkFBFZ7SqhJr4XSVt9QL9VBqjHo39Y" // <- UPDATE
+                    $0.clientKey = "VKEeO0P4vLlXk1ISxli7Z2L7abzZln6beKPjmmsK" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration
+    {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>)
+    {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Erin Byrd on 3/20/21.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
